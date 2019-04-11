7a944e7adfe317518545af52577ccad118f2b027
kmhosny <kmhosny@yahoo.com>
2019-04-11 12:07:40 +0200

    create c3d predict

--- c3d_predict.py
+++ c3d_predict.py
@@ -16,7 +16,7 @@ from video_data_generator import VideoDataGenerator
 from sklearn.model_selection import train_test_split
 
 WORK_DIR = '/home/kmhosny/datasets/UCF-101/'
-TRAIN_SPLIT_FILE = '/home/kmhosny/datasets/ucfTrainTestlist/trainlist01.txt'
+CLASS_IND = '/home/kmhosny/datasets/ucfTrainTestlist/classInd.txt'
 TEST_SPLIT_FILE = '/home/kmhosny/datasets/ucfTrainTestlist/testlist01.txt'
 CROP_SIZE = 112
 BATCH_SIZE = 16
@@ -25,76 +25,47 @@ BATCH_SIZE = 16
 num_of_frames = 16
 
 
-def read_file_ids(filename):
-    f = open(filename, 'r')
+def read_test_ids():
+    class_maping = {}
+    f_maping = open(CLASS_IND, 'r')
+    lines_maping = list(f_maping)
+    for i in range(len(lines_maping)):
+        line = lines_maping[i].strip('\n').split()
+        class_id = int(line[0]) - 1
+        class_name = line[1]
+        class_maping[class_name] = class_id
+    f_maping.close()
+
+    f = open(TEST_SPLIT_FILE, 'r')
     lines = list(f)
     its = range(len(lines))
     IDs = []
     labels = {}
     for it in its:
-        line = lines[it].strip('\n').split()
+        line = lines[it].strip('\n')
         dirname = line[0]
-        label = line[1]
         IDs.append(dirname)
-        labels[dirname] = int(label) - 1
+        class_name = dirname.split('//')[0]
+        labels[dirname] = class_maping[class_name]
     f.close()
-    print("Found %i files belonging to %i classes" %
-          (len(IDs), len(set(labels.values()))))
-    return IDs, labels
+    print("Found %i files for total of %i classes." %
+          (len(IDs), len(class_maping.keys())))
+    return IDs, labels, class_maping
 
 
-def split_train_validation(IDs, labels):
-    train_ids, validation_ids, _, _ = train_test_split(
-        IDs,
-        list(labels.keys()),
-        test_size=0.20,
-        random_state=len(set(labels.values())))
+def init_test_generator():
+    ids, ground_truth, class_maping = read_file_ids()
 
-    train_labels = {}
-    for label in train_ids:
-        train_labels[label] = labels[label]
-
-    validation_labels = {}
-    for label in validation_ids:
-        validation_labels[label] = labels[label]
-
-    return train_ids, train_labels, validation_ids, validation_labels
-
-
-def init_generators():
-    ids, labels = read_file_ids(TRAIN_SPLIT_FILE)
-    train_ids, train_labels, validation_ids, validation_labels = split_train_validation(
-        ids, labels)
-
-    train_datagen = VideoDataGenerator(
-        list_IDs=train_ids,
-        labels=train_labels,
+    test_datagen = VideoDataGenerator(
+        list_IDs=ids,
+        labels=ground_truth,
         crop_size=CROP_SIZE,
         batch_size=BATCH_SIZE,
         work_directory=WORK_DIR,
         n_channels=3,
-        n_classes=len(set(train_labels.values())))
+        n_classes=len(class_maping.keys()))
 
-    validation_datagen = VideoDataGenerator(
-        list_IDs=validation_ids,
-        labels=validation_labels,
-        crop_size=CROP_SIZE,
-        batch_size=BATCH_SIZE,
-        work_directory=WORK_DIR,
-        n_channels=3,
-        n_classes=len(set(train_labels.values())))
-    return train_datagen, validation_datagen
-
-
-def build_finetune_model(base_model, dropout, num_classes):
-    total_layers_len = len(base_model.layers)
-    for layer in base_model.layers[:total_layers_len - 1]:
-        layer.trainable = False
-
-
-#    base_model.pop()
-#   base_model.add(Dense(num_classes, activation='softmax', name='prediction'))
-    return base_model
+    return test_datagen
 
 
 def main():
@@ -106,27 +77,24 @@ def main():
     FC_LAYERS = [4096, 4096, 487]
     print(model_weight_filename, model_json_filename)
     model = model_from_json(open(model_json_filename, 'r').read())
-    dropout = 0.5
 
     print("[Info] Loading model weights...")
     model.load_weights(model_weight_filename)
-    model = build_finetune_model(model, dropout=dropout, num_classes=101)
 
     print("[Info] Loading model weights -- DONE!")
     model.compile(
         loss='mean_squared_error', optimizer='sgd', metrics=["accuracy"])
     filepath = "./models/c3d_UCF_finetune_weights-{epoch:02d}-{val_acc:.2f}.h5"
-    log_dir = "./logs"
-    checkpoint = ModelCheckpoint(
-        filepath, monitor="val_acc", verbose=1, mode='max')
+    log_dir = "./test_logs"
+
     board = TensorBoard(
         log_dir=log_dir,
         write_images=True,
         update_freq='epoch',
         histogram_freq=0)
-    callbacks_list = [checkpoint, board]
+    callbacks_list = [board]
 
-    history = model.fit_generator(
+    history = model.evaluate_generator(
         train_generator,
         validation_data=validation_generator,
         epochs=NUM_EPOCHS,
