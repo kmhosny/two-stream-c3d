861c8aad1c3d75c05dc6e3bf48c4f83d46a8c308
Karim Hosny <kmhosny89@yahoo.com>
2019-05-13 15:00:40 +0200

    enable gpu for static model

--- static_image_model.py
+++ static_image_model.py
@@ -1,3 +1,8 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+# The GPU id to use, usually either "0" or "1";
+os.environ["CUDA_VISIBLE_DEVICES"] = "2"
+
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Dense, Activation, Flatten, Dropout, GlobalAveragePooling2D
 from keras.models import Sequential, Model
