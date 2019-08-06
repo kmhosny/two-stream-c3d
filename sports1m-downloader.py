445e57439b84c99becd482e9eb0564bed0448d09
kmhosny <kmhosny@yahoo.com>
2019-08-06 15:06:57 +0200

    enable download on sorted subset

--- sports1m-downloader.py
+++ sports1m-downloader.py
@@ -7,6 +7,7 @@ import multiprocessing as mp
 from shutil import copy
 import datetime
 
+
 class MyLogger(object):
     def debug(self, msg):
         pass
@@ -22,10 +23,13 @@ def my_hook(d):
     if d['status'] == 'finished':
         print_with_date('Done downloading, now converting ...')
 
-def print_with_date(s):
-    print(str(datetime.datetime.now())+': '+s+'--'+str(mp.current_process()))
 
-SPORTS_FILE = cfg['SPORTS_1M_LIST']
+def print_with_date(s):
+    print(str(datetime.datetime.now()) + ': ' + s + '--' +
+          str(mp.current_process()))
+
+
+SPORTS_FILE = cfg['SPORTS_1M_LIST_SUBSET_SORTED']
 SPORTS_FILE_SUBSET = cfg['SPORTS_1M_LIST_SUBSET']
 SPORTS_FILE_SUBSET_SORTED = cfg['SPORTS_1M_LIST_SUBSET_SORTED']
 SPORTS_DATASET_DIR = cfg['SPORTS_DATASET_DIR']
@@ -40,21 +44,18 @@ for c in lines:
 flabels.close()
 
 mutex = mp.Lock()
-counters={}
+counters = {}
+
 
 def extract_info(l):
-    ydl_opts = {
-        'quiet': True,
-        'skip_download': True,
-        'logger': MyLogger()
-    }
+    ydl_opts = {'quiet': True, 'skip_download': True, 'logger': MyLogger()}
     print_with_date(l)
     splits = l.split()
     video_url = splits[0]
     ids = splits[1].split(',')
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         try:
-            info=ydl.extract_info(video_url)
+            info = ydl.extract_info(video_url)
         except Exception as e:
             print_with_date('downloading ' + video_url + 'failed')
             return
@@ -63,24 +64,25 @@ def extract_info(l):
     if duration > 0 and duration <= 90:
         mutex.acquire()
         print_with_date("------ACQUIRED LOCK-----")
-        write=False
+        write = False
         for i in ids:
             inti = int(i)
             if inti in counters and len(counters[inti]) < 150:
-                write=True
+                write = True
             elif not inti in counters:
                 counters[inti] = []
-                write=True
+                write = True
         if write:
             for i in ids:
                 inti = int(i)
                 if not inti in counters:
                     counters[inti] = []
                 counters[inti].append(l)
-            print_with_date('PUSHED '+video_url+" TO DATA OBJ")
+            print_with_date('PUSHED ' + video_url + " TO DATA OBJ")
         mutex.release()
         print_with_date("------RELEASED LOCK-----")
-    print_with_date('Video '+video_url+' is '+str(duration)+' long')
+    print_with_date('Video ' + video_url + ' is ' + str(duration) + ' long')
+
 
 def execution(l):
     ydl_opts = {
@@ -100,8 +102,9 @@ def execution(l):
         glob_res = glob.glob(SPORTS_DATASET_DIR + '/' + classes[inti] + '/' +
                              video_id + '*')
         if len(glob_res) > 0:
-            print_with_date(SPORTS_DATASET_DIR + '/' + classes[inti] + '/' + video_id +
-                  ' already exists as ', glob_res[0])
+            print_with_date(
+                SPORTS_DATASET_DIR + '/' + classes[inti] + '/' + video_id +
+                ' already exists as ', glob_res[0])
             src = glob_res[0]
             copy = True
             continue
@@ -124,8 +127,9 @@ def execution(l):
             print_with_date('downloading ' + video_id + 'failed')
     return ydl_opts['outtmpl']
 
+
 def sort_file(lines):
-    data={}
+    data = {}
     for l in lines:
         splits = l.split()
         ids = splits[1].split(',')
@@ -136,35 +140,38 @@ def sort_file(lines):
             data[inti].append(l)
     return data
 
+
 def write_data_obj(data, output):
     for k in data:
         for i in data[k]:
             output.write(i)
 
+
 def main():
-    # print_with_date('available cpus '+str(mp.cpu_count()))
-    # pool = mp.Pool(mp.cpu_count())
-    # print_with_date('initialised pool')
-    # f = open(SPORTS_FILE, 'r')
-    # print_with_date('readlines....')
-    # lines = f.readlines()
-    # print_with_date('staring forks.........')
-    # pool.map(extract_info, [l for l in lines])
-    # pool.close()
-    # f.close()
+    print_with_date('available cpus ' + str(mp.cpu_count()))
+    pool = mp.Pool(mp.cpu_count())
+    print_with_date('initialised pool')
+    f = open(SPORTS_FILE, 'r')
+    print_with_date('readlines....')
+    lines = f.readlines()
+    print_with_date('staring forks.........')
+    pool.map(execution, [l for l in lines])
+    pool.close()
+    f.close()
     #outfile = open(SPORTS_FILE_SUBSET, 'a+')
     #write_data_obj(counters, outfile)
-    f = open(SPORTS_FILE_SUBSET, 'r')
-    lines = f.readlines()
-    print_with_date('SORTING FILE')
-    counters = sort_file(lines)
-    print_with_date('DONE SORTING')
-    print_with_date('WRITING FILE')
-    outfile = open(SPORTS_FILE_SUBSET_SORTED, 'a+')
-    write_data_obj(counters, outfile)
-    print_with_date('DONE WRITING FILE')
-    outfile.close()
+    # f = open(SPORTS_FILE_SUBSET, 'r')
+    # lines = f.readlines()
+    # print_with_date('SORTING FILE')
+    # counters = sort_file(lines)
+    # print_with_date('DONE SORTING')
+    # print_with_date('WRITING FILE')
+    # outfile = open(SPORTS_FILE_SUBSET_SORTED, 'a+')
+    # write_data_obj(counters, outfile)
+    # print_with_date('DONE WRITING FILE')
+    # outfile.close()
     print(counters)
+
 
 if __name__ == "__main__":
     main()
