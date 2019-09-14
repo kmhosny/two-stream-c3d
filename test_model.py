2cbf53055f31d9a3e77e30c7f07f02ea8bbecee4
kmhosny <kmhosny@yahoo.com>
2019-09-14 18:44:52 +0200

    modify test model to test one network

--- test_model.py
+++ test_model.py
@@ -10,14 +10,17 @@ import matplotlib.pyplot as plt
 import c3d_model
 import sys
 import keras.backend as K
-from  keras.models import Model
+from keras.models import Model
+import one_network
+import random
 dim_ordering = K.image_dim_ordering()
 print("[Info] image_dim_order (from default ~/.keras/keras.json)={}".format(
-        dim_ordering))
+    dim_ordering))
 backend = dim_ordering
 
-h5model = 'sports1M_ucf_finetune_weights_tf.h5'
+h5model = 'one_network_scratch-l1-498-0.92.h5'
 jsonmodel = 'sports1M_ucf_finetune_weights_tf.json'
+
 
 def diagnose(data, verbose=True, label='input', plots=False, backend='tf'):
     # Convolution3D?
@@ -33,9 +36,9 @@ def diagnose(data, verbose=True, label='input', plots=False, backend='tf'):
         print("[Info] {}.shape={}".format(label, data.shape))
         for d in range(ndim):
             num_this_dim = data.shape[d]
-            if num_this_dim >= min_num_spatial_axes: # check for spatial axes
+            if num_this_dim >= min_num_spatial_axes:  # check for spatial axes
                 # just first, center, last indices
-                range_this_dim = [0, num_this_dim/2, num_this_dim - 1]
+                range_this_dim = [0, num_this_dim / 2, num_this_dim - 1]
             else:
                 # sweep all indices for non-spatial axes
                 range_this_dim = range(num_this_dim)
@@ -44,24 +47,22 @@ def diagnose(data, verbose=True, label='input', plots=False, backend='tf'):
                 sliced = np.transpose(data, new_dim)[i, ...]
                 print("[Info] {}, dim:{} {}-th slice: "
                       "(min, max, mean, std)=({}, {}, {}, {})".format(
-                              label,
-                              d, i,
-                              np.min(sliced),
-                              np.max(sliced),
-                              np.mean(sliced),
-                              np.std(sliced)))
+                          label, d, i, np.min(sliced), np.max(sliced),
+                          np.mean(sliced), np.std(sliced)))
         if plots:
             # assume (l, h, w, c)-shaped input
             if data.ndim != 4:
-                print("[Error] data (shape={}) is not 4-dim. Check data".format(
+                print(
+                    "[Error] data (shape={}) is not 4-dim. Check data".format(
                         data.shape))
                 return
             l, h, w, c = data.shape
             if l >= min_num_spatial_axes or \
                 h < min_num_spatial_axes or \
                 w < min_num_spatial_axes:
-                print("[Error] data (shape={}) does not look like in (l,h,w,c) "
-                      "format. Do reshape/transpose.".format(data.shape))
+                print(
+                    "[Error] data (shape={}) does not look like in (l,h,w,c) "
+                    "format. Do reshape/transpose.".format(data.shape))
                 return
             nrows = int(np.ceil(np.sqrt(data.shape[0])))
             # BGR
@@ -69,9 +70,9 @@ def diagnose(data, verbose=True, label='input', plots=False, backend='tf'):
                 for i in range(l):
                     mng = plt.get_current_fig_manager()
                     mng.resize(*mng.window.maxsize())
-                    plt.subplot(nrows, nrows, i + 1) # doh, one-based!
+                    plt.subplot(nrows, nrows, i + 1)  # doh, one-based!
                     im = np.squeeze(data[i, ...]).astype(np.float32)
-                    im = im[:, :, ::-1] # BGR to RGB
+                    im = im[:, :, ::-1]  # BGR to RGB
                     # force it to range [0,1]
                     im_min, im_max = im.min(), im.max()
                     if im_max > im_min:
@@ -89,7 +90,7 @@ def diagnose(data, verbose=True, label='input', plots=False, backend='tf'):
                     for i in range(l):
                         mng = plt.get_current_fig_manager()
                         mng.resize(*mng.window.maxsize())
-                        plt.subplot(nrows, nrows, i + 1) # doh, one-based!
+                        plt.subplot(nrows, nrows, i + 1)  # doh, one-based!
                         im = np.squeeze(data[i, ...]).astype(np.float32)
                         im = im[:, :, j]
                         # force it to range [0,1]
@@ -106,14 +107,11 @@ def diagnose(data, verbose=True, label='input', plots=False, backend='tf'):
                     #plt.waitforbuttonpress()
     elif data.ndim == 1:
         print("[Info] {} (min, max, mean, std)=({}, {}, {}, {})".format(
-                      label,
-                      np.min(data),
-                      np.max(data),
-                      np.mean(data),
-                      np.std(data)))
+            label, np.min(data), np.max(data), np.mean(data), np.std(data)))
         print("[Info] data[:10]={}".format(data[:10]))
 
     return
+
 
 def main():
     show_images = False
@@ -130,18 +128,20 @@ def main():
     print("[Info] Using backend={}".format(backend))
 
     if backend == 'th':
-        model_weight_filename = os.path.join(model_dir, 'sports1M_weights_th.h5')
-        model_json_filename = os.path.join(model_dir, 'sports1M_weights_th.json')
+        model_weight_filename = os.path.join(model_dir,
+                                             'sports1M_weights_th.h5')
+        model_json_filename = os.path.join(model_dir,
+                                           'sports1M_weights_th.json')
     else:
         model_weight_filename = os.path.join(model_dir, h5model)
         model_json_filename = os.path.join(model_dir, jsonmodel)
 
     print("[Info] Reading model architecture...")
-    model = model_from_json(open(model_json_filename, 'r').read())
+    #model = model_from_json(open(model_json_filename, 'r').read())
     #model = c3d_model.get_model(backend=backend)
-
+    model = one_network.deep_model()
     # visualize model
-    model_img_filename = os.path.join(model_dir, 'c3d_model.png')
+    model_img_filename = os.path.join(model_dir, 'one_network_model.png')
     if not os.path.exists(model_img_filename):
         from keras.utils import plot_model
         plot_model(model, to_file=model_img_filename)
@@ -152,73 +152,79 @@ def main():
     model.compile(loss='mean_squared_error', optimizer='sgd')
 
     print("[Info] Loading labels...")
-    with open('sports1m/labels.txt', 'r') as f:
+    with open(
+            '/media/kmhosny/01CFE6D64EF8ED00/datasets/ucfTrainTestlist/classInd.txt',
+            'r') as f:
         labels = [line.strip() for line in f.readlines()]
     print('Total labels: {}'.format(len(labels)))
 
     print("[Info] Loading a sample video...")
-    cap = cv2.VideoCapture('dM06AMFLsrc.mp4')
+    cap = cv2.VideoCapture('test_video.mp4')
 
     vid = []
     while True:
         ret, img = cap.read()
         if not ret:
             break
-        vid.append(cv2.resize(img, (171, 128)))
+        vid.append(cv2.resize(img, (112, 112)))
     vid = np.array(vid, dtype=np.float32)
-
     #plt.imshow(vid[2000]/256)
     #plt.show()
 
     # sample 16-frame clip
     #start_frame = 100
-    start_frame = 2000
+    start_frame = 5000
     print(vid.shape)
     X = vid[start_frame:(start_frame + 16), :, :, :]
+    image_index = random.randint(0, len(vid) - 1)
+    im = vid[image_index]
     print(X.shape)
     #diagnose(X, verbose=True, label='X (16-frame clip)', plots=show_images)
 
     # subtract mean
-    mean_cube = np.load('models/train01_16_128_171_mean.npy')
-    mean_cube = np.transpose(mean_cube, (1, 2, 3, 0))
+    #mean_cube = np.load('models/train01_16_128_171_mean.npy')
+    #mean_cube = np.transpose(mean_cube, (1, 2, 3, 0))
     #diagnose(mean_cube, verbose=True, label='Mean cube', plots=show_images)
-    X -= mean_cube
+    #X -= mean_cube
     #diagnose(X, verbose=True, label='Mean-subtracted X', plots=show_images)
 
     # center crop
-    X = X[:, 8:120, 30:142, :] # (l, h, w, c)
-    print(X.shape)
+    #X = X[:, 8:120, 30:142, :]  # (l, h, w, c)
+    print('after crop ' + str(X.shape))
     #diagnose(X, verbose=True, label='Center-cropped X', plots=show_images)
 
     if backend == 'th':
-        X = np.transpose(X, (3, 0, 1, 2)) # input_shape = (3,16,112,112)
+        X = np.transpose(X, (3, 0, 1, 2))  # input_shape = (3,16,112,112)
     else:
-        pass                              # input_shape = (16,112,112,3)
+        pass  # input_shape = (16,112,112,3)
 
     # get activations for intermediate layers if needed
     inspect_layers = [
-    #    'fc6',
-    #    'fc7',
-        ]
+        #    'fc6',
+        #    'fc7',
+    ]
     for layer in inspect_layers:
-        int_model = c3d_model.get_int_model(model=model, layer=layer, backend=backend)
+        int_model = c3d_model.get_int_model(
+            model=model, layer=layer, backend=backend)
         int_output = int_model.predict_on_batch(np.array([X]))
         int_output = int_output[0, ...]
-        print("[Debug] at layer={}: output.shape={}".format(layer, int_output.shape))
-        diagnose(int_output,
-                 verbose=True,
-                 label='{} activation'.format(layer),
-                 plots=diagnose_plots,
-                 backend=backend)
+        print("[Debug] at layer={}: output.shape={}".format(
+            layer, int_output.shape))
+        diagnose(
+            int_output,
+            verbose=True,
+            label='{} activation'.format(layer),
+            plots=diagnose_plots,
+            backend=backend)
 
     # inference
     print(model.summary())
-    intermediate_layer_model = Model(inputs=model.input,
-                                     outputs=model.get_layer(name='pool5').output)
-    intermediate_output = intermediate_layer_model.predict(np.array([X]))
+    #    intermediate_layer_model = Model(
+    #        inputs=model.input, outputs=model.get_layer(name='pool5').output)
+    #    intermediate_output = intermediate_layer_model.predict(np.array([X]))
     print("before prediction", np.array([X]).shape)
-    output = model.predict_on_batch(np.array([X]))
-    print(intermediate_output.shape)
+    output = model.predict_on_batch([np.array([X]), np.array([im])])
+    #    print(intermediate_output.shape)
 
     # show results
     print('Saving class probabilitities in probabilities.png')
@@ -230,10 +236,12 @@ def main():
     print('Corresponding label: {}'.format(labels[output[0].argmax()]))
 
     # sort top five predictions from softmax output
-    top_inds = output[0].argsort()[::-1][:5]  # reverse sort and take five largest items
+    top_inds = output[0].argsort(
+    )[::-1][:5]  # reverse sort and take five largest items
     print('\nTop 5 probabilities and labels:')
     for i in top_inds:
         print('{1}: {0:.5f}'.format(output[0][i], labels[i]))
+
 
 if __name__ == '__main__':
     main()
